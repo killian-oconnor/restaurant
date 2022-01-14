66cb38251401e9111c0b8f1d06773ea1259915fb
killian-oconnor <80956433+killian-oconnor@users.noreply.github.com>
2022-01-14 20:24:58 +0000

    added some additional content and image to homepage

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import pizzaPic from './we-love-pizza.jpg';
+import kitchenPic from './kitchen.jpg';
 import margheritaPic from './margherita.png';
 import pepperoniPic from './pepperoni.png';
 import veggiePic from './veggie.png';
@@ -27,9 +27,43 @@ const HomeTabModule = (() => {
 
     const pizzaBlurb = document.createElement("p");
     pizzaBlurb.textContent = "Pizza tastes good because it has richness from meats, fat and cheese, the appealing texture of a crispy crust, an acidic and slightly sweet sauce, and a host of savory toppings. If you throw on a bunch of mushrooms, it packs an awesome umami punch as well. Pizza smells great too.";
-    pizzaBlurb.textContent += "I guess you could say that pizza appeals to all of your senses, there isn’t much to dislike about it.";
+    pizzaBlurb.textContent += " I guess you could say that pizza appeals to all of your senses, there isn’t much to dislike about it.";
     pizzaBlurb.classList.add("text-lg","text-white","leading-tight");
     blurbHolder.appendChild(pizzaBlurb);
+
+    const kitchenBlurbHolder = document.createElement("div");
+    kitchenBlurbHolder.classList = "grid grid-cols-2 gap-4 m-5";
+
+    const kitchenBlurbTextHolder = document.createElement("div");
+    //kitchenBlurbTextHolder.classList = "col-span-2";
+
+    const kitchenBlurbText = document.createElement("p");
+    //kitchenBlurbText.textContent = "Whisk together the water, yeast, olive oil, and salt. Let the yeast foam for 5 minutes. Add in 3 cups of flour and use your dough hook to knead. Knead for a minimum of 5 minutes, until the dough is smooth and just slightly tacky, but not sticky. Add the additional ½ cup of flour, if needed, and knead until smooth.Place the dough in a large, well oiled bowl. Cover with plastic wrap and put in a warm place to proof for 1 hour, or wrap the dough in plastic wrap and store in your refrigerator until you're ready to use it. If using the dough from the refrigerator, let it come to room temperature before working with it."
+    kitchenBlurbText.innerHTML = `<p style='margin-right:0cm;margin-left:0cm;font-size:15px;font-family:"Calibri",sans-serif;margin-top:0cm;margin-bottom:10.0pt;line-height:115%;'>The <strong>history of pizza</strong> begins in antiquity, when various ancient cultures produced basic flatbreads with several toppings.</p>
+    <p style='margin-right:0cm;margin-left:0cm;font-size:15px;font-family:"Calibri",sans-serif;margin-top:0cm;margin-bottom:10.0pt;line-height:115%;'>A precursor of pizza was probably the focaccia, a flatbread known to the Romans as <em>panis focacius</em>, to which toppings were then added. Modern pizza evolved from similar flatbread dishes in Naples, Italy, in the 18th or early 19th century.</p>
+    <p style='margin-right:0cm;margin-left:0cm;font-size:15px;font-family:"Calibri",sans-serif;margin-top:0cm;margin-bottom:10.0pt;line-height:115%;'>The word <em>pizza</em> was first documented in 997 AD in Gaeta and successively in different parts of Central and Southern Italy. Pizza was mainly eaten in Italy and by emigrants from there. This changed after World War II when Allied troops stationed in Italy came to enjoy pizza along with other Italian foods.</p>
+    <p style='margin-right:0cm;margin-left:0cm;font-size:15px;font-family:"Calibri",sans-serif;margin-top:0cm;margin-bottom:10.0pt;line-height:115%;'>&nbsp;</p>`
+    kitchenBlurbText.classList = pizzaBlurb.classList;
+    kitchenBlurbTextHolder.appendChild(kitchenBlurbText);
+
+    const kitchenImageHolder = document.createElement("div");
+    const kitchenImage = new Image();
+    kitchenImage.src = kitchenPic;
+    kitchenImage.classList.add("w-auto","mx-auto");
+    kitchenImageHolder.appendChild(kitchenImage);
+
+    kitchenBlurbHolder.appendChild(kitchenBlurbTextHolder);
+    kitchenBlurbHolder.appendChild(kitchenImageHolder);
+    blurbHolder.appendChild(kitchenBlurbHolder);
+
+
+
+    
+    // <div class="grid grid-cols-3 gap-4">
+    //     <div class="...">01</div>
+    //     <div class="col-span-2 ...">04</div>
+    // </div>
+
 
     // const imgHolder = document.createElement("div");
     // imgHolder.classList.add("m-auto","flex","flex-col-reverse","md:flex-row","justify-center","bg-white","rounded","shadow-lg","overflow-hidden","max-w-fit");
